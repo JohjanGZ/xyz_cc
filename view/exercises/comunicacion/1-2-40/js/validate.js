771a3c7993bfc03a49107e2ca718b38c6ca55052
Jhon <jhonmaldonadoarmijos99@gmail.com>
2019-11-05 10:08:52 -0500

    Ejercicios desde el 38 al 46

---
+++ view/exercises/comunicacion/1-2-40/js/validate.js
@@ -0,0 +1,32 @@
+var r = 0;
+console.log(r);
+
+var ul = document.querySelector('.contenedor-elementos');
+for (var i = ul.children.length; i >= 0; i--) {
+    ul.appendChild(ul.children[Math.random() * i | 0]);
+}
+
+$(".pieza").draggable({
+    start: function() {
+        element = $(this).attr("alt");
+        elementid = $(this);
+        console.log(element);
+
+    }
+});
+// uA
+$(".contenedor-faltante").droppable({
+    classes: {
+        "ui-droppable-active": "ui-state-active",
+        "ui-droppable-hover": "ui-state-hover"
+    },
+    drop: function(event, ui) {
+        $(this).css("background-color", "rgba(139, 195, 74, 0.14)");
+        element2 = $(this).attr("alt");
+        elementid.css("background-color", "transparent");
+        if (element == element2) {
+            r++;
+        }
+        element.removeClass("pieza");
+    }
+});
