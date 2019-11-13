700d09124b63464bd03bdb3aa45563211d109a26
Jhon <jhonmaldonadoarmijos99@gmail.com>
2019-11-13 18:08:24 -0500

    validate

--- view/exercises/comunicacion/1-5-17/js/validate.js
+++ view/exercises/comunicacion/1-5-17/js/validate.js
@@ -13,11 +13,6 @@ $('.letras span').click(function() {
     // Estilos a span
     $('.letras span').css({ "transform": "scale(1)", "border": "2px solid #37D3F7", "box-shadow": "none", "background": "transparent" });
     $(this).css({ "border": "solid #37D3F7", "box-shadow": "0 0 4px #00000057", "background": "#B6ECFF" });
-<<<<<<< HEAD
-
-=======
-    console.log(letra)
->>>>>>> 5e4c2eb7897d46857d4fc5d8bf8fb76b728cac73
 });
 $('.palabra').click(function() {
     $(this).css({ "border": "solid #37D3F7", "background": "#B6ECFF" });
@@ -39,12 +34,7 @@ $('.palabra').click(function() {
         }
     }
     $(this).text(letra);
-<<<<<<< HEAD
 
 
 
-=======
-    console.log(letra);
-    console.log(r);
->>>>>>> 5e4c2eb7897d46857d4fc5d8bf8fb76b728cac73
 });
