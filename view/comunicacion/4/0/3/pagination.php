965a775bc2cf633b8948e15bf6d8d2931fe966a6
Jhon <jhonmaldonadoarmijos99@gmail.com>
2019-12-18 11:03:21 -0500

    Ejercicios

--- view/comunicacion/4/0/3/pagination.php
+++ view/comunicacion/4/0/3/pagination.php
@@ -1,16 +1,6 @@
 <script type="text/javascript">
    
-    function Page_7(){
-        start_7();
-        inicio();
-        count = 3;
-    }
-   function  Page_8(){
-        start_8();
-        inicio();
-         count = 3;
-    }
-    function Page_9(){
+   function Page_9(){
         start_9();
         inicio();
         count = 3;
@@ -18,70 +8,99 @@
    function  Page_10(){
         start_10();
         inicio();
-         count = 3;
+        count = 3;
+    }
+    function Page_11(){
+        start_11();
+        inicio();
+        count = 3;
+    }
+    function  Page_12(){
+        start_12();
+        inicio();
+        count = 3;
+    }
+    function Page_13(){
+        start_13();
+        inicio();
+        count = 3;
     }
 
-    var cant=4;
+    var cant=5;
     var cal= 20/20;
     var ruta="../../../../exercises/comunicacion/";
     ////////////// 2do 
-    // ----------  para iniciar y reiniciar ejercicios sin que afecte el cronometro -----------------------
-    
-    function start_7(){
-        $("#ventana").load(ruta+'3-0-7/index.php', 
-        { 
-        next: "Page_8()", 
-        procesar:"result_tipo_3_0_7()",
-        titulo:"<center><h5><span>Arrastra</span> cada sílaba en el recuadro donde corresponde para completar las palabras.</h5></center>",
-        restaurar:"start_7()",
-        dir:ruta,
-        cod: "3-0-7",
-        nota:cal
-        }
-        );
-        return false;
-    };
-    function start_8(){
-        $("#ventana").load(ruta+'3-0-8/index.php', 
-        { 
-        next: "Page_9()", 
-        procesar:"result_tipo_3_0_8()",
-        titulo:"<center><h5><span>Separa</span> en sílabas el nombre de las siguientes imágenes.</h5></center>",
-        restaurar:"start_8()",
-        dir:ruta,
-        cod: "3-0-8",
-        nota:cal
-        }
-        );
-        return false;
-    };
+            // ----------  para iniciar y reiniciar ejercicios sin que afecte el cronometro -----------------------
+
     function start_9(){
-        $("#ventana").load(ruta+'3-0-9/index.php', 
-        { 
+        $("#ventana").load(ruta+'4-0-9/index.php', 
+        {
         next: "Page_10()", 
-        procesar:"result_tipo_3_0_9()",
-        titulo:"<center><h5><span>Selecciona</span> el número de casillas que tiene cada palabra.</h5></center>",
+        procesar:"result_tipo_4_0_9()",
+        titulo:"<center><h5><span>Marca</span> <i>Verdadero</i> si la sílaba resaltada es tónica y <i>Falso</i> si no lo es.</h5></center>",
         restaurar:"start_9()",
         dir:ruta,
-        cod: "3-0-9",
+        cod: "4-0-9",
         nota:cal
         }
         );
-        return false;
+        return false;  
     };
     function start_10(){
-        $("#ventana").load(ruta+'3-0-10/index.php', 
-        { 
-        next: "resultado()", 
-        procesar:"result_tipo_3_0_10()",
-        titulo:"<center><h5><span>Ordena</span> las sílabas para formar la palabra correctamente.</h5></center>",
+        $("#ventana").load(ruta+'4-0-10/index.php', 
+        {
+        next: "Page_11()", 
+        procesar:"result_tipo_4_0_10()",
+        titulo:"<center><h5><span>Rellena</span> los recuadros con la palabra correctamente silabeada de cada grupo.</h5></center>",
         restaurar:"start_10()",
         dir:ruta,
-        cod: "3-0-10",
+        cod: "4-0-10",
         nota:cal
         }
         );
-        return false;
+        return false;  
+    };
+    function start_11(){
+        $("#ventana").load(ruta+'4-0-11/index.php', 
+        {
+        next: "Page_12()", 
+        procesar:"result_tipo_4_0_11()",
+        titulo:"<center><h5><span>Selecciona</span> la sílaba tónica de cada palabra.</h5></center>",
+        restaurar:"start_11()",
+        dir:ruta,
+        cod: "4-0-11",
+        nota:cal
+        }
+        );
+        return false;  
+    };
+    function start_12(){
+        $("#ventana").load(ruta+'4-0-12/index.php', 
+        {
+        next: "Page_13()", 
+        procesar:"result_tipo_4_0_12()",
+        titulo:"<center><h5><span>Selecciona</span> el número de sílabas átonas que hay en cada palabra.</h5></center>",
+        restaurar:"start_12()",
+        dir:ruta,
+        cod: "4-0-12",
+        nota:cal
+        }
+        );
+        return false;  
+    };
+    function start_13(){
+        $("#ventana").load(ruta+'4-0-13/index.php', 
+        {
+        next: "resultado()",
+        procesar:"result_tipo_4_0_13()",
+        titulo:"<center><h5><span>Lee</span> correctamente las palabras y <span>arrástralas</span> en la oración correspondiente.</h5></center>",
+        restaurar:"start_13()",
+        dir:ruta,
+        cod: "4-0-13",
+        nota:cal
+        }
+        );
+        return false;  
     };
     function resultado(){
         $("#ventana").load('../../../../exercises/resultado/resultado.php');
