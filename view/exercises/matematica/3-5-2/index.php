d7a41409b7feee069207964fdf6fcd89c40b0b6e
Guianphier <guianphierlb@gmail.com>
2019-12-30 17:54:28 -0500

    5

---
+++ view/exercises/matematica/3-5-2/index.php
@@ -0,0 +1,273 @@
+<?php require('../../../tools/var/variables.php'); ?>
+
+<head>
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1.0" />
+    <link rel="stylesheet" type="text/css" href="<?= $dir ?>/css/styles.css">
+</head>
+<style type="text/css">
+
+</style>
+<?=$titulo?>
+<?php 
+    $numbers = array('53,9','19','35,8','437,5','397,6','963,2','5,39','1,9','3,58','43,75','39,76','96,32','0,539','0,19','0,358','4,375','3,976','9,632');
+?>
+<body>
+    <div class="container-two">
+        <div class="grid-container">
+            <div class="grid-item he">
+                <h6>÷ ↱</h6>
+            </div>
+            <div class="grid-item he">
+                <h6>539</h6>
+            </div>
+            <div class="grid-item he">
+                <h6>190</h6>
+            </div>
+            <div class="grid-item">
+                <h6>358</h6>
+            </div>
+            <div class="grid-item">
+                <h6>4 375</h6>
+            </div>
+            <div class="grid-item">
+                <h6>3 976</h6>
+            </div>
+            <div class="grid-item imageCaballo">
+                <h6>9 632</h6>
+            </div>
+
+            <div class="grid-item">
+                <h6>10</h6>
+            </div>
+            <div class="grid-item">
+                <select class="slc seleccion" alt="53,9">
+                    <option value="" disabled selected></option>
+                    <?php
+                        for ($i=0; $i < count($numbers); $i++) { 
+                            echo "<option value='$numbers[$i]'>$numbers[$i]</option>";
+                        }
+                    ?>
+                </select>
+            </div>
+            <div class="grid-item">
+                <select class="slc seleccion" alt="19">
+                    <option value="" disabled selected></option>
+                    <?php
+                        for ($i=0; $i < count($numbers); $i++) { 
+                            echo "<option value='$numbers[$i]'>$numbers[$i]</option>";
+                        }
+                    ?>
+                </select>
+            </div>
+            <div class="grid-item">
+                <select class="slc seleccion" alt="35,8">
+                    <option value="" disabled selected></option>
+                    <?php
+                        for ($i=0; $i < count($numbers); $i++) { 
+                            echo "<option value='$numbers[$i]'>$numbers[$i]</option>";
+                        }
+                    ?>
+                </select>
+            </div>
+            <div class="grid-item">
+                <select class="slc seleccion" alt="437,5">
+                    <option value="" disabled selected></option>
+                    <?php
+                        for ($i=0; $i < count($numbers); $i++) { 
+                            echo "<option value='$numbers[$i]'>$numbers[$i]</option>";
+                        }
+                    ?>
+                </select>
+            </div>
+            <div class="grid-item">
+                <select class="slc seleccion" alt="397,6">
+                    <option value="" disabled selected></option>
+                    <?php
+                        for ($i=0; $i < count($numbers); $i++) { 
+                            echo "<option value='$numbers[$i]'>$numbers[$i]</option>";
+                        }
+                    ?>
+                </select>
+            </div>
+            <div class="grid-item">
+                <select class="slc seleccion" alt="963,2">
+                    <option value="" disabled selected></option>
+                    <?php
+                        for ($i=0; $i < count($numbers); $i++) { 
+                            echo "<option value='$numbers[$i]'>$numbers[$i]</option>";
+                        }
+                    ?>
+                </select>
+            </div>
+
+            <div class="grid-item">
+                <h6>100</h6>
+            </div>
+            <div class="grid-item">
+                <select class="slc seleccion" alt="5,39">
+                    <option value="" disabled selected></option>
+                    <?php
+                        for ($i=0; $i < count($numbers); $i++) { 
+                            echo "<option value='$numbers[$i]'>$numbers[$i]</option>";
+                        }
+                    ?>
+                </select>
+            </div>
+            <div class="grid-item">
+                <select class="slc seleccion" alt="1,9">
+                    <option value="" disabled selected></option>
+                    <?php
+                        for ($i=0; $i < count($numbers); $i++) { 
+                            echo "<option value='$numbers[$i]'>$numbers[$i]</option>";
+                        }
+                    ?>
+                </select>
+            </div>
+            <div class="grid-item">
+                <select class="slc seleccion" alt="3,58">
+                    <option value="" disabled selected></option>
+                    <?php
+                        for ($i=0; $i < count($numbers); $i++) { 
+                            echo "<option value='$numbers[$i]'>$numbers[$i]</option>";
+                        }
+                    ?>
+                </select>
+            </div>
+            <div class="grid-item">
+                <select class="slc seleccion" alt="43,75">
+                    <option value="" disabled selected></option>
+                    <?php
+                        for ($i=0; $i < count($numbers); $i++) { 
+                            echo "<option value='$numbers[$i]'>$numbers[$i]</option>";
+                        }
+                    ?>
+                </select>
+            </div>
+            <div class="grid-item">
+                <select class="slc seleccion" alt="39,76">
+                    <option value="" disabled selected></option>
+                    <?php
+                        for ($i=0; $i < count($numbers); $i++) { 
+                            echo "<option value='$numbers[$i]'>$numbers[$i]</option>";
+                        }
+                    ?>
+                </select>
+            </div>
+            <div class="grid-item">
+                <select class="slc seleccion" alt="96,32">
+                    <option value="" disabled selected></option>
+                    <?php
+                        for ($i=0; $i < count($numbers); $i++) { 
+                            echo "<option value='$numbers[$i]'>$numbers[$i]</option>";
+                        }
+                    ?>
+                </select>
+            </div>
+            
+            <div class="grid-item">
+                <h6>1 000</h6>
+            </div>
+            <div class="grid-item">
+                <select class="slc seleccion" alt="0,539">
+                    <option value="" disabled selected></option>
+                    <?php
+                        for ($i=0; $i < count($numbers); $i++) { 
+                            echo "<option value='$numbers[$i]'>$numbers[$i]</option>";
+                        }
+                    ?>
+                </select>
+            </div>
+            <div class="grid-item">
+                <select class="slc seleccion" alt="0,19">
+                    <option value="" disabled selected></option>
+                    <?php
+                        for ($i=0; $i < count($numbers); $i++) { 
+                            echo "<option value='$numbers[$i]'>$numbers[$i]</option>";
+                        }
+                    ?>
+                </select>
+            </div>
+            <div class="grid-item">
+                <select class="slc seleccion" alt="0,358">
+                    <option value="" disabled selected></option>
+                    <?php
+                        for ($i=0; $i < count($numbers); $i++) { 
+                            echo "<option value='$numbers[$i]'>$numbers[$i]</option>";
+                        }
+                    ?>
+                </select>
+            </div>
+            <div class="grid-item">
+                <select class="slc seleccion" alt="4,375">
+                    <option value="" disabled selected></option>
+                    <?php
+                        for ($i=0; $i < count($numbers); $i++) { 
+                            echo "<option value='$numbers[$i]'>$numbers[$i]</option>";
+                        }
+                    ?>
+                </select>
+            </div>
+            <div class="grid-item">
+                <select class="slc seleccion" alt="3,976">
+                    <option value="" disabled selected></option>
+                    <?php
+                        for ($i=0; $i < count($numbers); $i++) { 
+                            echo "<option value='$numbers[$i]'>$numbers[$i]</option>";
+                        }
+                    ?>
+                </select>
+            </div>
+            <div class="grid-item">
+                <select class="slc seleccion" alt="9,632">
+                    <option value="" disabled selected></option>
+                    <?php
+                        for ($i=0; $i < count($numbers); $i++) { 
+                            echo "<option value='$numbers[$i]'>$numbers[$i]</option>";
+                        }
+                    ?>
+                </select>
+            </div>
+        </div>
+    </div>
+    <!-- Respuesta -->
+    <div id="modal1" class="modal">
+        <div class="modal-content">
+            <center>
+                <h4>Respuesta correcta</h4>
+                <img class="materialboxed" width="80%" src="<?= $dir ?>/img/respuesta.png">
+            </center>
+        </div>
+        <div class="modal-footer">
+            <a href="#!" class="modal-close waves-effect waves-green btn-flat">Listo!</a>
+        </div>
+    </div>
+</body>
+
+
+<script src="../../../../../js/core.js"></script>
+<?php require('../../../tools/botones/botones.php');?><script>
+$("#next").attr("onclick", "<?=$next?>");
+</script>
+<script type="text/javascript" src="<?= $dir ?>/js/validate.js"></script>
+<script type="text/javascript">
+// Validar
+function result_tipo_3_5_2() {
+    var min = $('#Minutos').text();
+    var seg = $('#Segundos').text();
+    var milseg = $('#Centesimas').text();
+    var tiempo = min + ":" + seg + ":" + milseg;
+    r = 0;
+    seleccion_lista();
+    if (r == 18 ) {
+        correcto();
+        localStorage.setItem("Nota<?=$cod?>", nota);
+        localStorage.setItem("Time<?=$cod?>", tiempo);
+
+    } else {
+        incorrecto();
+        localStorage.setItem("Nota<?=$cod?>", "0");
+        localStorage.setItem("Time<?=$cod?>", tiempo);
+    }
+
+}
+</script>
--- view/matematica/3/4/3/pagination.php
+++ view/matematica/3/4/3/pagination.php
@@ -1,16 +1,36 @@
 <script type="text/javascript">
+function Page_20() {
+    start_20();
+    inicio();
+    count = 3;
+}
+
 function Page_21() {
     start_21();
     inicio();
     count = 3;
 }
+
 function Page_22() {
     start_22();
     inicio();
     count = 3;
 }
+
 function Page_23() {
     start_23();
+    inicio();
+    count = 3;
+}
+
+function Page_24() {
+    start_24();
+    inicio();
+    count = 3;
+}
+
+function Page_25() {
+    start_25();
     inicio();
     count = 3;
 }
@@ -19,11 +39,24 @@ var cal = cant / cant;
 console.log(cant);
 var ruta = "../../../../exercises/matematica/";
 ////////////// 2do 
+function start_20() {
+    $("#ventana").load(ruta + '3-4-20/index.php', {
+        next: "Page_21()",
+        procesar: "result_tipo_3_4_20()",
+        titulo: "<center><h5><span>Responde</span> las siguientes preguntas:</h5></center>",
+        restaurar: "start_20()",
+        dir: ruta,
+        cod: "3-4-20",
+        nota: cal
+    });
+    return false;
+};
+
 function start_21() {
     $("#ventana").load(ruta + '3-4-21/index.php', {
         next: "Page_22()",
-        procesar: "result_tipo_1_4_21()",
-        titulo: "<center><h5><span class='ftitulo'>Realiza</span> la descomposición de los siguientes números indicando  de cada uno el valor en unidades.<br> <span class='ftitulo'>Guíate</span> del ejemplo. </h5></center>", 
+        procesar: "result_tipo_3_4_21()",
+        titulo: "<center><h5><span>Responde</span> las siguientes preguntas:</h5></center>",
         restaurar: "start_21()",
         dir: ruta,
         cod: "3-4-21",
@@ -31,11 +64,12 @@ function start_21() {
     });
     return false;
 };
+
 function start_22() {
     $("#ventana").load(ruta + '3-4-22/index.php', {
-        next: "start_23()",
-        procesar: "result_tipo_1_4_22()",
-        titulo: "<center><h5><span class='ftitulo'>Realiza</span> la descomposición de los siguientes números indicando el valor posicional de cada una de sus cifras. <br> Guíate del ejemplo. </h5></center>", 
+        next: "Page_23()",
+        procesar: "result_tipo_3_4_22()",
+        titulo: "<center><h5><span>Observa</span> el gráfico y <span>selecciona</span> todas las oraciones que son correctas.</h5></center>",
         restaurar: "start_22()",
         dir: ruta,
         cod: "3-4-22",
@@ -43,14 +77,41 @@ function start_22() {
     });
     return false;
 };
+
 function start_23() {
     $("#ventana").load(ruta + '3-4-23/index.php', {
-        next: "resultado()",
-        procesar: "result_tipo_1_4_23()",
-        titulo: "<center><h5><span class='ftitulo'>Selecciona</span> la pelota que cuesta 43 soles.</h5></center>", 
+        next: "Page_24()",
+        procesar: "result_tipo_3_4_23()",
+        titulo: "<center><h5><span>Observa</span> el gráfico y <span>responde</span></h5></center>",
         restaurar: "start_23()",
         dir: ruta,
         cod: "3-4-23",
+        nota: cal
+    });
+    return false;
+};
+
+function start_24() {
+    $("#ventana").load(ruta + '3-4-24/index.php', {
+        next: "Page_25()",
+        procesar: "result_tipo_3_4_24()",
+        titulo: "<center><h5><span>Selecciona</span> el diagrama que represente la indicación de cada enunciado. Ten en cuenta que cada círculo representa un total de 100 personas.</h5></center>",
+        restaurar: "start_24()",
+        dir: ruta,
+        cod: "3-4-24",
+        nota: cal
+    });
+    return false;
+};
+
+function start_25() {
+    $("#ventana").load(ruta + '3-4-25/index.php', {
+        next: "resultado()",
+        procesar: "result_tipo_3_4_25()",
+        titulo: "<center><h5><span>Selecciona</span> el diagrama que represente la indicación de cada enunciado. Ten en cuenta que cada círculo representa un total de 100 personas.</h5></center>",
+        restaurar: "start_25()",
+        dir: ruta,
+        cod: "3-4-25",
         nota: cal
     });
     return false;
