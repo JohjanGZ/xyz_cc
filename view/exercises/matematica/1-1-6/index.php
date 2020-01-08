70d9e62b9a8f9dd59c6d1dbf4bcc7c62e5964da1
Guianphier <guianphierlb@gmail.com>
2020-01-08 11:56:47 -0500

    s

--- view/exercises/matematica/1-1-6/index.php
+++ view/exercises/matematica/1-1-6/index.php
@@ -18,7 +18,6 @@
             <div class="grid-item">
                 <div class="flex-box">
                     <img src="<?= $dir ?>/img/1.png">
-<<<<<<< HEAD:view/exercises/matematica/1-1-13/index.php
                     <div class="contenedor" alt="3"></div>
                     <img src="<?= $dir ?>/img/2.png">
                 </div>
@@ -54,50 +53,31 @@
                 <div class="flex-box">
                     <img src="<?= $dir ?>/img/9.png">
                     <div class="contenedor" alt="3"></div>
-=======
-                    <div class="caja contenedor" alt="1"></div>
->>>>>>> a62b8f102345509f10fe5fa0dc2b4abe60cd7fd7:view/exercises/matematica/1-1-6/index.php
                 </div>
             </div>
 
             <div class="grid-item">
                 <div class="flex-box">
-<<<<<<< HEAD:view/exercises/matematica/1-1-13/index.php
                     <div class="contenedor" alt="1"></div>
                     <img src="<?= $dir ?>/img/10.png">
-=======
-                    <img src="<?= $dir ?>/img/2.png">
-                    <div class="caja contenedor" alt="8"></div>
->>>>>>> a62b8f102345509f10fe5fa0dc2b4abe60cd7fd7:view/exercises/matematica/1-1-6/index.php
                 </div>
             </div>
             <div class="grid-item">
                 <div class="flex-box">
-<<<<<<< HEAD:view/exercises/matematica/1-1-13/index.php
                     <img src="<?= $dir ?>/img/11.png">
                     <div class="contenedor" alt="7"></div>
                     <img src="<?= $dir ?>/img/12.png">
-=======
-                    <img src="<?= $dir ?>/img/3.png">
-                    <div class="caja contenedor" alt="7"></div>
->>>>>>> a62b8f102345509f10fe5fa0dc2b4abe60cd7fd7:view/exercises/matematica/1-1-6/index.php
                 </div>
             </div>
             <div class="grid-item">
                 <div class="flex-box">
-<<<<<<< HEAD:view/exercises/matematica/1-1-13/index.php
                     <img src="<?= $dir ?>/img/13.png">
                     <div class="contenedor" alt="6"></div>
-=======
-                    <img src="<?= $dir ?>/img/4.png">
-                    <div class="caja contenedor" alt="5"></div>
->>>>>>> a62b8f102345509f10fe5fa0dc2b4abe60cd7fd7:view/exercises/matematica/1-1-6/index.php
                 </div>
             </div>
         </div>
 
         <div class="contenedor-silabas">
-<<<<<<< HEAD:view/exercises/matematica/1-1-13/index.php
             <ul class="opciones">
                 <?php
                 $opciones = array('3','5','6','5','7','3','1','7','6','8','10','4');
@@ -107,39 +87,6 @@
                           </li>";
                 }
             ?>
-=======
-            <ul class="opciones aleatorio">
-                <li id="1" alt="1" class="pieza">
-                    <span>1</span>
-                </li>
-                <li id="2" alt="2" class="pieza">
-                    <span>2</span>
-                </li>
-                <li id="3" alt="3" class="pieza">
-                    <span>3</span>
-                </li>
-                <li id="4" alt="4" class="pieza">
-                    <span>4</span>
-                </li>
-                <li id="5" alt="5" class="pieza">
-                    <span>5</span>
-                </li>
-                <li id="6" alt="6" class="pieza">
-                    <span>6</span>
-                </li>
-                <li id="7" alt="7" class="pieza">
-                    <span>7</span>
-                </li>
-                <li id="8" alt="8" class="pieza">
-                    <span>8</span>
-                </li>
-                <li id="9" alt="9" class="pieza">
-                    <span>9</span>
-                </li>
-                <li id="10" alt=" 10" class="pieza">
-                    <span>10</span>
-                </li>
->>>>>>> a62b8f102345509f10fe5fa0dc2b4abe60cd7fd7:view/exercises/matematica/1-1-6/index.php
             </ul>
         </div>
     </div>
@@ -171,11 +118,7 @@ function result_tipo_1_1_6() {
     var milseg = $('#Centesimas').text();
     var tiempo = min + ":" + seg + ":" + milseg;
 
-<<<<<<< HEAD:view/exercises/matematica/1-1-13/index.php
     if (r == 9) {
-=======
-    if (r == 4) {
->>>>>>> a62b8f102345509f10fe5fa0dc2b4abe60cd7fd7:view/exercises/matematica/1-1-6/index.php
         localStorage.setItem("Nota<?=$cod?>", nota);
         localStorage.setItem("Time<?=$cod?>", tiempo);
         correcto();
